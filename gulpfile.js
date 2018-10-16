e27ed2bc0784d78f42e265ebfc2ccf7878f971c6
Matt Graham <grahammatt@live.com>
2018-10-16 00:13:06 -0400

    feat: add player movement
    Player can move. No collision with enemies or border is implemented yet.

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,14 @@
-<<<<<<< HEAD
 /*jshint esversion: 6*/
 
-=======
->>>>>>> de0bced70061c829b12dec03f90c8bd3b1c0515a
 const gulp = require("gulp");
 const sass = require("gulp-sass");
 const autoprefixer = require("gulp-autoprefixer");
 const browserSync = require('browser-sync').create();
 
 gulp.task("default", ["styles"], function() {
-<<<<<<< HEAD
   gulp.watch("styles/**/*.scss", ["styles"]);
   gulp.watch("index.html").on("change", browserSync.reload);
   gulp.watch("scripts/*.js").on("change", browserSync.reload);
-=======
-  gulp.watch("sass/**/*.scss", ["styles"]);
->>>>>>> de0bced70061c829b12dec03f90c8bd3b1c0515a
   browserSync.init({
     server: "./"
   });
@@ -23,22 +16,14 @@ gulp.task("default", ["styles"], function() {
 
 gulp.task("styles", function() {
   gulp
-<<<<<<< HEAD
     .src("styles/**/*.scss")
-=======
-    .src("sass/**/*.scss")
->>>>>>> de0bced70061c829b12dec03f90c8bd3b1c0515a
     .pipe(sass().on("error", sass.logError))
     .pipe(
       autoprefixer({
         browsers: ["last 2 versions"]
       })
     )
-<<<<<<< HEAD
     .pipe(gulp.dest("./styles"))
-=======
-    .pipe(gulp.dest("./css"))
->>>>>>> de0bced70061c829b12dec03f90c8bd3b1c0515a
     .pipe(browserSync.stream());
 
 });
--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 /*jshint esversion: 6*/
 
 // Enemies our player must avoid
+// TODO: convert to es6 class
 var Enemy = function(x, y, speed) {
   // Variables applied to each of our instances go here,
   // we've provided one for you to get started
@@ -33,23 +34,40 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+
+// TODO: convert to es6 class
 var Player = function(x, y) {
   this.x = x;
   this.y = y;
   this.sprite = 'images/char-boy.png';
 };
-Player.prototype.update = function(dt) {
-  // You should multiply any movement by the dt parameter
-  // which will ensure the game runs at the same speed for
-  // all computers.
+
+Player.prototype.update = function(x = 0, y = 0) {
+  if (x != 0 || y != 0) {
+    this.x += x;
+    this.y += y;
+  }
 };
 
 Player.prototype.render = function() {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-Player.prototype.handleInput = function() {
-
+Player.prototype.handleInput = function(keyPress) {
+  switch (keyPress) {
+    case `up`:
+      this.update(0, -83);
+      break;
+    case `down`:
+      this.update(0, 83);
+      break;
+    case `left`:
+      this.update(-101);
+      break;
+    case `right`:
+      this.update(101);
+      break;
+  }
 };
 
 // Now instantiate your objects.
@@ -65,13 +83,13 @@ allEnemies.push(...[
   new Enemy(-234, 215, 40)
 ]);
 // Place the player object in a variable called player
-var player = new Player(202, 400);
+let player = new Player(202, 400);
 // TODO: fix player positioning
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-  var allowedKeys = {
+  let allowedKeys = {
     37: 'left',
     38: 'up',
     39: 'right',
